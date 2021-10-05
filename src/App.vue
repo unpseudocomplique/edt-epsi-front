1591d2d35b8498c12bbe74b62505deee9664b8db
Mathieu <mathieu@bestconsultant.fr>
2021-10-05 16:39:15 +0200

    Add some ui

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,25 @@
 <script setup>
 import { BASE_URL } from '@/config'
+import { ref } from '@vue/reactivity';
+import Lesson from './components/lesson.vue';
 console.log('BASE_URL', BASE_URL);
-fetch(`${BASE_URL}/edt/mathieu.janio/10-04-21`).then(response => {
-  console.log('data :', response);
-})
+const agenda = ref()
+fetch(`http://localhost:8080/edt/mathieu.janio/10-04-21`)
+  .then(response => response.json()).then(response => {
+    console.log(response);
+    agenda.value = response
+  })
 </script>
 
 <template>
   <div class="p-5">
     <h1 class="text-2xl">EDT EPSI</h1>
+    <div class="flex flex-wrap justify-evenly">
+      <div v-for="lessons in agenda" class="m-4">
+        <h2>{{ lessons.day }}</h2>
+        <Lesson class="mt-2" v-for="lesson in lessons.lessons" :lesson="lesson" />
+      </div>
+    </div>
   </div>
 </template>
 
---
+++ src/components/lesson.vue
@@ -0,0 +1,25 @@
+<script setup>
+const props = defineProps({
+    lesson: Object
+})
+</script>
+
+<template>
+    <div class="p-4 lesson-item rounded-lg shadow">
+        <p class="font-bold">{{ lesson.matiere }}</p>
+        <div>
+            <i>{{ lesson.heure }}</i>
+            <div class="flex justify-between">
+                <p>{{ lesson.prof }}</p>
+                <p>{{ lesson.salle }}</p>
+            </div>
+        </div>
+    </div>
+</template>
+<style>
+.lesson-item {
+    width: 400px;
+    max-width: 80vw;
+    background-color: v-bind(lesson.color);
+}
+</style>
