d98cb9b3846881d0fe3ac39daad888698937fe15
Mathieu <mathieu@bestconsultant.fr>
2021-10-25 16:08:48 +0200

    add pwa support

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,18 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [vue()],
+	plugins: [
+		vue(),
+		VitePWA({
+			includeAssets: ['favicon.ico', 'robot.txt'],
+			manifest: {
+				name: 'EDT',
+				short_name: 'EDT',
+				theme_color: '#374151'
+			},
+			registerType: 'autoUpdate'
+		})
+	],
 	resolve: {
 		alias: {
 			'@/': `${path.resolve(__dirname, 'src')}/`
