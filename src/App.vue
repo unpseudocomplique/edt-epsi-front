f92e3d065d5c6a6442997034990866e900635ab9
Mathieu <mathieu@bestconsultant.fr>
2021-10-25 14:57:53 +0200

    add select range

--- src/App.vue
+++ src/App.vue
@@ -1,74 +1,131 @@
 <script setup>
 import { BASE_URL } from '@/config'
-import { ref, watch } from 'vue';
+import { ref, watch, reactive } from 'vue';
 import dayjs from 'dayjs'
 import 'dayjs/locale/fr'
 import Lesson from './components/lesson.vue';
 import { DatePicker } from 'v-calendar';
-import { addColorsToLessons } from './services';
+import { addColorsToLessons, getDatesBetweenDates } from './services';
 import LoaderLesson from './components/loaderLesson.vue';
+import { useStorage } from '@vueuse/core'
+import { useFetch } from './services/useFetch'
 const agenda = ref([])
-const date = ref(new Date())
-const user = 'mathieu.janio'
-const isLoading = ref(false)
+const user = useStorage('username', '')
+const range = ref({
+  start: new Date(),
+  end: new Date()
+})
+const input = 'YYYY-MM-DD h:mm A'
 
-const fetchData = (newDate, newUser) => {
-  const formatedDay = dayjs(newDate).format('MM-DD-YYYY')
-  isLoading.value = true
-  fetch(`${BASE_URL}/edt/${newUser}/${formatedDay}`)
-    .then(response => response.json()).then(response => {
-      agenda.value = addColorsToLessons(response)
-      console.log(agenda.value);
-    }).finally(() => isLoading.value = false)
+
+const fetchData = (newRange, newUser) => {
+  const dates = getDatesBetweenDates(newRange.start, newRange.end)
+  console.log('dates', dates);
+  agenda.value = dates.map(currentDate => {
+    const formatedDay = dayjs(currentDate).format('MM-DD-YYYY')
+    const request = useFetch({ method: 'GET', url: `${BASE_URL}/edt/${newUser}/${formatedDay}` })
+    return {
+      formatedDay,
+      request
+    }
+  })
 }
-watch(date,
+watch(range,
   (newDate, prevDate) => {
-    console.log('new data...');
-    fetchData(newDate, user)
+    fetchData(newDate, user.value)
   }
 )
-fetchData(date.value, user)
+// fetchData(date.value, user.value)
+fetchData(range.value, user.value)
 </script>
 
 <template>
   <div class="p-5">
-    <h1 class="text-2xl">EDT EPSI</h1>
-    <DatePicker class="inline-block h-full" v-model="date">
-      <template v-slot="{ inputValue, togglePopover }">
-        <div class="flex items-center">
-          <button class="p-2 rounded-l dark:bg-gray-800" @click="togglePopover()">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              class="w-4 h-4 fill-current"
-            >
-              <path
-                d="M1 4c0-1.1.9-2 2-2h14a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V4zm2 2v12h14V6H3zm2-6h2v2H5V0zm8 0h2v2h-2V0zM5 9h2v2H5V9zm0 4h2v2H5v-2zm4-4h2v2H9V9zm0 4h2v2H9v-2zm4-4h2v2h-2V9zm0 4h2v2h-2v-2z"
-              />
-            </svg>
-          </button>
-          <input
-            @click="togglePopover"
-            :value="inputValue"
-            class="dark:bg-gray-800 text-white w-full py-1 px-2 appearance-none rounded-r"
-            readonly
-          />
+    <h1 class="text-2xl text-gray-100">EDT EPSI</h1>
+    <div class="flex items-end mt-3">
+      <label class="flex flex-col w-min mr-2">
+        prénom.nom
+        <input
+          placeholder="mathieu.janio"
+          class="bg-gray-800 p-1 rounded mt-2"
+          type="text"
+          v-model="user"
+        />
+      </label>
+      <div>
+        <span class="block text-gray-100 text-sm text-left mb-2">Select Range</span>
+        <DatePicker v-model="range" is-range>
+          <template v-slot="{ inputValue, inputEvents, isDragging }">
+            <div class="flex flex-col sm:flex-row justify-start items-center">
+              <div class="relative flex-grow">
+                <svg
+                  class="text-gray-100 w-4 h-full mx-2 absolute pointer-events-none"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
+                  />
+                </svg>
+                <input
+                  class="flex-grow pl-8 pr-2 py-1 bg-gray-800 text-white rounded w-full"
+                  :value="inputValue.start"
+                  v-on="inputEvents.start"
+                />
+              </div>
+              <span class="flex-shrink-0 m-2">
+                <svg class="w-4 h-4 stroke-current text-gray-100" viewBox="0 0 24 24">
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M14 5l7 7m0 0l-7 7m7-7H3"
+                  />
+                </svg>
+              </span>
+              <div class="relative flex-grow">
+                <svg
+                  class="text-gray-100 w-4 h-full mx-2 absolute pointer-events-none"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
+                  />
+                </svg>
+                <input
+                  class="flex-grow pl-8 pr-2 py-1 bg-gray-800 text-white w-full"
+                  :value="inputValue.end"
+                  v-on="inputEvents.end"
+                />
+              </div>
+            </div>
+          </template>
+        </DatePicker>
+      </div>
+    </div>
+    <div class="flex flex-wrap justify-start flex-row mt-10">
+      <div class="mt-10 mx-5" v-for="day in agenda">
+        <h2 class="text-xl">{{ day.formatedDay }}</h2>
+        <div v-if="!day.request.loading">
+          <Lesson v-for="lesson in day.request.data" class="mt-2" :lesson="lesson" />
         </div>
-      </template>
-    </DatePicker>
-    <div class="flex flex-wrap justify-evenly flex-col mt-10">
-      <transition name="fade" mode="out-in">
-        <div v-if="isLoading">
+        <div v-else="isLoading">
           <LoaderLesson class="mt-2" v-for="item in [0, 1, 2, 3]" :key="item" />
         </div>
-        <div v-else>
-          <h2 class="text-xl">{{ dayjs(date).locale('fr').format('dddd D MMMM') }}</h2>
-          <div v-if="agenda.length > 1" class="m-1">
-            <Lesson v-for="lesson in agenda" class="mt-2" :lesson="lesson" />
-          </div>
-          <div class="text-xl my-4" v-else>Aucun cours trouvé</div>
-        </div>
-      </transition>
+        <div
+          class="text-xl my-4"
+          v-if="!day.request.loading && day.request.data < 1"
+        >Aucun cours trouvé</div>
+      </div>
     </div>
   </div>
 </template>
--- src/services/index.ts
+++ src/services/index.ts
@@ -12,7 +12,7 @@ export const addColorsToLessons = (lessons) => {
 			config.room !== lesson.room ||
 			config.teacher !== lesson.teacher ||
 			config.name !== lesson.name
-            
+
 		if (index === 0 || isSameLessonAsSaved) {
 			config.room = lesson.room
 			config.teacher = lesson.teacher
@@ -27,4 +27,15 @@ export const addColorsToLessons = (lessons) => {
 			color: colors[config.index]
 		}
 	})
+}
+
+export const getDatesBetweenDates = (startDate: Date, endDate: Date) => {
+	let dates = []
+	//to avoid modifying the original date
+	const theDate = new Date(startDate)
+	while (theDate <= endDate) {
+		dates = [...dates, new Date(theDate)]
+		theDate.setDate(theDate.getDate() + 1)
+	}
+	return dates
 }
